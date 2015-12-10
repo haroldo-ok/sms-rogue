00eb913086052c791fc47cfac71f4f7ea65ae240
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-12-10 17:23:56 -0200

    Removed the now redundant batch files

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Build gfx.c from gfx folder
-folder2c gfx gfx
-sdcc -c -mz80 --std-sdcc99 gfx.c
-echo Build Main
-sdcc -c -mz80 --std-sdcc99 sms-rogue.c
-echo Linking
-sdcc -mz80 --data-loc 0xC000 sms-rogue.rel SMSlib/SMSlib.rel gfx.rel
-ihx2sms sms-rogue.ihx sms-rogue.sms
